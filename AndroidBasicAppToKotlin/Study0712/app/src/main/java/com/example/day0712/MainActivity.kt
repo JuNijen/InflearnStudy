6aa7982cb4fcdf450ac913e06f24d7aaf69437d2
juNijen <wjstldms0823@naver.com>
2021-07-12 20:39:52 +0900

    [EDIT] fin 0712 study

--- AndroidBasicAppToKotlin/Study0712/app/src/main/java/com/example/day0712/MainActivity.kt
+++ AndroidBasicAppToKotlin/Study0712/app/src/main/java/com/example/day0712/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.day0712
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +12,76 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initObjects()
+    }
+
+    lateinit var tvResult : TextView
+    lateinit var etNumber1 : EditText
+    lateinit var etNumber2 : EditText
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMul : Button
+    lateinit var btnDiv : Button
+
+    private fun initObjects()
+    {
+        tvResult = findViewById(R.id.result_view)
+        etNumber1 = findViewById(R.id.editTextNumber1)
+        etNumber2 = findViewById(R.id.editTextNumber2)
+        btnAdd = findViewById(R.id.buttonAdd)
+        btnSub = findViewById(R.id.buttonSub)
+        btnMul = findViewById(R.id.buttonMul)
+        btnDiv = findViewById(R.id.buttonDiv)
+
+
+        btnAdd.setOnClickListener {
+            clickAdd()
+        }
+        btnSub.setOnClickListener {
+            clickSub()
+        }
+        btnMul.setOnClickListener {
+            clickMul()
+        }
+        btnDiv.setOnClickListener {
+            clickDiv()
+        }
+    }
+
+    private fun clickAdd()
+    {
+        var numA : Int = Integer.parseInt(etNumber1.text.toString())
+        var numB : Int = Integer.parseInt(etNumber2.text.toString())
+        var resultString = String.format("%s%d", getString(R.string.text_result), numA+numB)
+
+        tvResult.text = resultString
+    }
+
+    private fun clickSub()
+    {
+        var numA : Int = Integer.parseInt(etNumber1.text.toString())
+        var numB : Int = Integer.parseInt(etNumber2.text.toString())
+        var resultString = String.format("%s%d", getString(R.string.text_result), numA-numB)
+
+        tvResult.text = resultString
+    }
+
+    private fun clickMul()
+    {
+        var numA : Int = Integer.parseInt(etNumber1.text.toString())
+        var numB : Int = Integer.parseInt(etNumber2.text.toString())
+        var resultString = String.format("%s%d", getString(R.string.text_result), numA*numB)
+
+        tvResult.text = resultString
+    }
+
+    private fun clickDiv()
+    {
+        var numA : Int = Integer.parseInt(etNumber1.text.toString())
+        var numB : Int = Integer.parseInt(etNumber2.text.toString())
+        var resultString = String.format("%s%d", getString(R.string.text_result), numA/numB)
+
+        tvResult.text = resultString
     }
 }
